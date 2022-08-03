fc9a90f22d893617283d28b1df6373297f66ed96
dhiraj giri <99414791+dhirajgiri3@users.noreply.github.com>
2022-08-04 02:56:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+const bar = document.getElementById('bar');
+const nav = document.getElementById('navbar');
+const cross = document.getElementById('cross');
+
+if (bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
+} 
+
+if (cross) {
+    cross.addEventListener('click', () => {
+        nav.classList.remove('active');
+    })
+}
